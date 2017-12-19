cabc27c643c5efab82bf54b786126e4611e2bac6
roman.furdychka@gmail.com <roman.furdychka@gmail.com>
2017-12-19 11:23:32 +0200

    Vitalik

--- Tests/src/org/testpurposes/Main.java
+++ Tests/src/org/testpurposes/Main.java
@@ -3,7 +3,7 @@ package org.testpurposes;
 public class Main {
     public static void main(String[] args) {
         System.out.println("11000");
-        System.out.println("12000");
+        System.out.println("15");
     }
 
 }
