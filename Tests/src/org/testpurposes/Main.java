96585cdea5e076186705e954c8e3e3d9c611aae4
roman.furdychka@gmail.com <roman.furdychka@gmail.com>
2017-12-19 12:14:56 +0200

    rom002

--- Tests/src/org/testpurposes/Main.java
+++ Tests/src/org/testpurposes/Main.java
@@ -3,7 +3,7 @@ package org.testpurposes;
 public class Main {
     public static void main(String[] args) {
         System.out.println("001");
-        System.out.println("Romik02");
+        System.out.println("Romik01");
     }
 
 }
