c1d1f2e5ccf1298bd54247bea90d29fcd3102fe3
roman.furdychka@gmail.com <roman.furdychka@gmail.com>
2017-12-19 14:12:23 +0200

    Change Rom2

--- Tests/src/org/testpurposes/Main.java
+++ Tests/src/org/testpurposes/Main.java
@@ -2,23 +2,6 @@ package org.testpurposes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("001");
-        System.out.println("001");
-        System.out.println("001");
-        System.out.println("001");
-        System.out.println("001");
-        System.out.println("001");
-        System.out.println("001");
-        System.out.println("001");
-        System.out.println("Romik01  ");
-
         System.out.println("Romik");
-        System.out.println("Romik");
-
-        System.out.println("002");
-        System.out.println("Vitalik02");
-
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
     }
 }
