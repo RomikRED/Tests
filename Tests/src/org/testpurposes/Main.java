bf1793b9a5e718e33a8053e68c6e03d1f7ee9286
roman.furdychka@gmail.com <roman.furdychka@gmail.com>
2017-12-19 12:25:03 +0200

    vit003

--- Tests/src/org/testpurposes/Main.java
+++ Tests/src/org/testpurposes/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Romik01");
         System.out.println("002");
         System.out.println("Vitalik02");
+
+        System.out.println("Vitalik");
     }
 }
