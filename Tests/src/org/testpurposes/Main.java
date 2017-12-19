10b678412afd94b7999d4115d12d5a2c4a88d12c
Vitalik <py4ina@gmail.com>
2017-12-19 14:33:06 +0200

    vitalik

--- Tests/src/org/testpurposes/Main.java
+++ Tests/src/org/testpurposes/Main.java
@@ -3,5 +3,25 @@ package org.testpurposes;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
+        System.out.println("Vitalik");
     }
 }
