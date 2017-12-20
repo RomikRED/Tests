b30cf0cd7dad63a4d20f6774dda25f7724bbaef6
roman.furdychka@gmail.com <roman.furdychka@gmail.com>
2017-12-20 10:51:24 +0200

    Merge remote-tracking branch 'remotes/origin/Vitalik' into developer
    # Conflicts:
    #	Tests/src/org/testpurposes/Main.java

--- Tests/src/org/testpurposes/Main.java
+++ Tests/src/org/testpurposes/Main.java
@@ -2,6 +2,6 @@ package org.testpurposes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Dev_00:Rom_02:Vit_02");
+        System.out.println("Dev_00:Rom_02:Rom_03:Vit_02");
     }
 }
