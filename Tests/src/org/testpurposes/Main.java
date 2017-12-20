eda6f837269f8bfc5667ad3c737f4c4658b50d57
roman.furdychka@gmail.com <roman.furdychka@gmail.com>
2017-12-20 10:19:15 +0200

    Commit0

--- Tests/src/org/testpurposes/Main.java
+++ Tests/src/org/testpurposes/Main.java
@@ -2,26 +2,6 @@ package org.testpurposes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
-        System.out.println("Vitalik");
+        System.out.println("Vit");
     }
 }
